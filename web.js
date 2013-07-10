896a7d0444777c51b38aa8bd3e7fc96d8efbb1c2
BarbDean <bdean1201@aol.com>
2013-07-10 12:26:34 -0600

    latest

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  var fs = require('fs');
+  var fileContents = fs.readFileSync("./index.html",'utf8'); 
+  response.send(fileContents);
 });
 
 var port = process.env.PORT || 5000;
